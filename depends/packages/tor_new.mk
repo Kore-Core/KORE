33c4f32f0f2706c7ef90c2d3f545047e7ad012fd
Lico <lico@coreblockchain.com.br>
2018-09-06 11:10:33 -0300

    adding current tor as dependency

--- depends/packages/tor_new.mk
+++ depends/packages/tor_new.mk
@@ -1,8 +1,8 @@
 package=tor
-$(package)_version=0.2.5.1-alpha
-$(package)_download_path=https://archive.torproject.org/tor-package-archive
+$(package)_version=0.3.2.9
+$(package)_download_path=https://dist.torproject.org
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=3100d8f0ccb296e82f1d3602266d4dcde801daace82af74f5f27ee2fca7bb4a8
+$(package)_sha256_hash=435a7b91aa98d8b1a0ac1f60ca30c0ff3665b18a02e570bab5fe27935829160f
 $(package)_dependencies=zlib libevent openssl
 
 define $(package)_set_vars
@@ -23,9 +23,14 @@ define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/lib && \
   cp $($(package)_build_dir)/src/or/libtor.a $($(package)_staging_prefix_dir)/lib/libtor.a && \
   cp $($(package)_build_dir)/src/common/libor.a $($(package)_staging_prefix_dir)/lib/libor.a && \
+  cp $($(package)_build_dir)/src/common/libor-ctime.a $($(package)_staging_prefix_dir)/lib/libor-ctime.a && \
   cp $($(package)_build_dir)/src/common/libor-crypto.a $($(package)_staging_prefix_dir)/lib/libor-crypto.a && \
   cp $($(package)_build_dir)/src/common/libor-event.a $($(package)_staging_prefix_dir)/lib/libor-event.a && \
-	cp $($(package)_build_dir)/src/common/libcurve25519_donna.a $($(package)_staging_prefix_dir)/lib/libcurve25519_donna.a
+	cp $($(package)_build_dir)/src/common/libcurve25519_donna.a $($(package)_staging_prefix_dir)/lib/libcurve25519_donna.a && \
+  cp $($(package)_build_dir)/src/trunnel/libor-trunnel.a $($(package)_staging_prefix_dir)/lib/libor-trunnel.a && \
+	cp $($(package)_build_dir)/src/ext/ed25519/donna/libed25519_donna.a $($(package)_staging_prefix_dir)/lib/libed25519_donna.a && \
+  cp $($(package)_build_dir)/src/ext/ed25519/ref10/libed25519_ref10.a $($(package)_staging_prefix_dir)/lib/libed25519_ref10.a && \
+  cp $($(package)_build_dir)/src/ext/keccak-tiny/libkeccak-tiny.a $($(package)_staging_prefix_dir)/lib/libkeccak-tiny.a
 endef
 
 define $(package)_postprocess_cmds
